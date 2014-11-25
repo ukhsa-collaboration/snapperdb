693a06da9e0ed9d561b33f1117f945bc8ed727e6
Phil Ashton <philip.ashton@phe.gov.uk>
2014-11-25 12:25:07 +0000

    added update_distance_matrix to SnapperDB, about to start optimisation

--- snpdb/__init__.py
+++ snpdb/__init__.py
@@ -11,6 +11,7 @@ import errno
 from gbru_vcf import Vcf
 import pickle
 import re
+import numpy as np
 
 class SNPdb:
     """
@@ -500,12 +501,18 @@ class SNPdb:
             strain1_good_var = self.strains_snps[strain1]
             strain1_ig_pos = self.get_bad_pos_for_strain_update_matrix(cur, strain1)
             for strain2 in data_list:
-                if strain1 != strain2 and strain2 not in seen_strain:    
+                if strain1 != strain2 and strain2 not in seen_strain:
                     strain2_good_var = self.strains_snps[strain2]
                     strain2_ig_pos = self.get_bad_pos_for_strain_update_matrix(cur, strain2)
                     #getunion of bad_pos
-                    all_bad_pos = set(strain1_ig_pos) | set(strain2_ig_pos)
-                    #getsymmetric differecne of variants
+                    #all_bad_pos = set(strain1_ig_pos) | set(strain2_ig_pos)
+
+                    all_bad_pos = np.union1d(np.asarray(strain1_ig_pos), np.asarray(strain2_ig_pos))
+
+
+
+
+                    #getsymmetric difference of variants
                     all_var = set(strain1_good_var) ^ set(strain2_good_var)
                     diff = 0
                     for var_id in all_var:
