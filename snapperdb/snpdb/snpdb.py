fe4de13d48e175dcf977094cf5a37f0df639b4da
Phil Ashton <philip.ashton@phe.gov.uk>
2015-04-30 21:04:19 +0100

    tweaking

--- snapperdb/snpdb/snpdb.py
+++ snapperdb/snpdb/snpdb.py
@@ -553,21 +553,20 @@ class SNPdb:
         # os.system('rm -rf {0}/update_list*'.format(this_dir))
         # os.system('rm -rf {0}/update_matrix_*'.format(this_dir))
 
-        args.now
 
         for i, each in enumerate(self.chunks(update_strain, args.hpc)):
-            with open('{0}/{1}.update_list_{2}'.format(scripts_dir, args.now, i), 'w') as fo:
+            with open('{0}/{1}.{3}.update_list_{2}'.format(scripts_dir, args.now, i, self.snpdb_name), 'w') as fo:
                 for x in each:
                     fo.write(x + '\n')
-        with open('{0}/{1}.short_strain_list'.format(scripts_dir, args.now), 'w') as fo:
+        with open('{0}/{1}.{2}.short_strain_list'.format(scripts_dir, args.now, self.snpdb_name), 'w') as fo:
             for x in short_strain_list:
                 fo.write(x + '\n')
 
-        with open('{0}/{1}.strain_list'.format(scripts_dir, args.now), 'w') as fo:
+        with open('{0}/{1}.{2}.strain_list'.format(scripts_dir, args.now, self.snpdb_name), 'w') as fo:
             for x in strain_list:
                 fo.write(x + '\n')
 
-        res = sorted(glob.glob('{0}/{1}.update_list*'.format(scripts_dir, args.now)))
+        res = sorted(glob.glob('{0}/{1}.{2}.update_list*'.format(scripts_dir, args.now, self.snpdb_name)))
 
         for i, update_list in enumerate(res):
             command = ('#! /bin/bash\n'
@@ -582,11 +581,11 @@ class SNPdb:
                        ' qsub_to_check_matrix -c {4}'
                        ' -l {5}/{7}.strain_list'
                        ' -s {5}/{7}.short_strain_list'
-                       ' -u {5}/{6}\n'.format(logs_dir, logs_dir, snpdb, i, args.config_file, scripts_dir, update_list, args.now))
+                       ' -u {6}\n'.format(logs_dir, logs_dir, snpdb, i, args.config_file, scripts_dir, update_list, args.now))
 
-            with open('{0}/update_matrix_{1}.sh'.format(this_dir, i), 'w') as fo:
+            with open('{0}/{2}.{3}.update_mat_{1}.sh'.format(scripts_dir, i, args.now, self.snpdb_name), 'w') as fo:
                 fo.write(command)
-            os.system('qsub {0}/update_matrix_{1}.sh'.format(this_dir, i))
+            os.system('qsub {0}/{2}.{3}.update_mat_{1}.sh'.format(this_dir, i, args.now, self.snpdb_name))
 
 
             # os.system('chmod u+x {0}/update_matrix_{1}.sh'.format(this_dir, i))
